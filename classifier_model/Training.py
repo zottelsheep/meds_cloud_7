665f5d01ef02311373bbcb66f4f771575099476d
zottelsheep <mail@zottelsheep.de>
2022-11-16 17:47:19 +0100

    Refactor: Use relative paths

--- classifier_model/Training.py
+++ classifier_model/Training.py
@@ -5,14 +5,15 @@ Created on Fri Nov 11 17:43:58 2022
 @author: Elias
 """
 
-import os
+from pathlib import Path
+
 import numpy
 import tensorflow as tf
 from tensorflow.keras.applications import Xception 
 import tf2onnx
 
 
-raw_image = tf.keras.utils.load_img("C:\\Users\\Elias\\Desktop\\Studium\\7Semester\\Medizinische_Cloud_und_Verteilte_Systeme\\KI_Modell\\dog.jpg",target_size=[299,299])
+raw_image = tf.keras.utils.load_img(Path("./dog.jpg"),target_size=[299,299])
 #Transform image into array:
 array_image =  tf.keras.utils.img_to_array(raw_image)
 #push image into vector:
@@ -22,11 +23,11 @@ model = Xception(weights='imagenet')
 
 preds = model.predict(image)
 print('Keras Predicted:', tf.keras.applications.imagenet_utils.decode_predictions(preds, top=3)[0])
-model.save(os.path.join("./tmp", model.name))
+model.save(Path("./tmp") / model.name)
 print('Model gespeichert')
 
 spec = (tf.TensorSpec((None, 299, 299, 3), tf.float32, name="input"),)
-output_path = model.name + ".onnx"
+output_path = Path(model.name) / ".onnx"
 
 model_proto, _ = tf2onnx.convert.from_keras(model, input_signature=spec, opset=13, output_path=output_path)
 output_names = [n.name for n in model_proto.graph.output]
