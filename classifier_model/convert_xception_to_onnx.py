0b39b65e9ecb971263247784dc06e1503e60b892
Elias <s190544@th-ab.de>
2022-11-16 19:21:09 +0100

    start model comparison

--- classifier_model/convert_xception_to_onnx.py
+++ classifier_model/convert_xception_to_onnx.py
@@ -11,6 +11,8 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.applications import Xception 
 import tf2onnx
+from keras import applications
+import onnxruntime as ort
 
 # Convert Model
 print('Convert Model Xception to onnx...')
@@ -29,13 +31,22 @@ print('Done!')
 print('Testing Model...')
 
 # Load dog image
-raw_image = tf.keras.utils.load_img(Path("./dog.jpg"),target_size=[299,299])
+raw_image = tf.keras.utils.load_img(Path("dog.jpg"),target_size=[299,299])
 #Transform image into array:
-image = np.array([tf.keras.utils.img_to_array(raw_image)])
-
+image_as_array = np.array([tf.keras.utils.img_to_array(raw_image)])
+image = applications.xception.preprocess_input(image_as_array)
+#Test original keras model predictions
 preds = mdl.predict(image)
 decoded_predictions = tf.keras.applications.imagenet_utils.decode_predictions(preds, top=3)[0]
-print(f'Original Prediction: {decoded_predictions}')
+print(f'Original (keras) Prediction: {decoded_predictions}')
+#Test onnx model predicitons
+
+# WIP
+#onnx_mdl = ort.InferenceSession("xception.onnx", providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
+#input_shape = onnx_mdl.get_inputs()[0].shape
+#print(input_shape, image_as_array.shape)
+#label = onnx_mdl.run(['label'],
+#                     {'features': image_as_array})
 
 #TODO: @Elias check onnx model-prediction against normal prediction
 
