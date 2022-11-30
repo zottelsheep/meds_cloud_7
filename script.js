9126495550fd099b64903859b0f2cd3fde48a8c8
Maxim <Rayanov>
2022-11-30 18:09:34 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const target = document.getElementById("drop_target");
+const dragText = document.getElementById("drag_header");
+const testIcon = document.getElementById("some_test");
+
+function drag_over() {
+    dragText.textContent = "Release to upload";
+    target.classList.add("acitve");
+    testIcon.trigger = "loop";
+}
+
+function drag_leave() {
+    dragText.textContent = "Drag & drop a picture";
+    target.classList.remove("active");
+    testIcon.trigger = "empty"
+}
+
