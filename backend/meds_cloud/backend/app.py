08cc6c1c1601d02e63eecf40d57eadc669a88d2e
zottelsheep <mail@zottelsheep.de>
2022-12-14 23:03:54 +0100

    Chore: Change default port to 5001

--- backend/meds_cloud/backend/app.py
+++ backend/meds_cloud/backend/app.py
@@ -92,7 +92,8 @@ def main():
     print(("* Loading Keras model and Flask starting server..."
            "please wait until server has fully started"))
     load_model()
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',
+            port=5001)
 
 
 # if this is the main thread of execution first load the model and
