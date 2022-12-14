cf56c8c5f28e4d87597dc9b4d8ac9f33387fe6fe
Maxim <Rayanov>
2022-12-14 10:32:56 +0100

    Feat: Test implementation fileupload

--- classifier_frontend/static/script.js
+++ classifier_frontend/static/script.js
@@ -1,4 +1,4 @@
-const target = document.getElementById("drop_target");
+const target2 = document.getElementById("drop_target");
 const containerTarget = document.getElementById("target_container");
 const dragText = document.getElementById("drag_header");
 const testIcon = document.getElementById("some_test");
@@ -6,22 +6,24 @@ const inputButton = document.getElementById("input");
 
 function drag_over() {
     dragText.textContent = "Release to upload";
-    target.classList.add("active");
+    target2.classList.add("active");
     containerTarget.classList.add("active");
     testIcon.trigger = "loop";
 }
 
 function drag_leave() {
     dragText.textContent = "Drag & drop a picture";
-    target.classList.remove("active");
+    target2.classList.remove("active");
     containerTarget.classList.remove("active");
     testIcon.trigger = "empty";
 }
 
 function upload_file(event) {
     dragText.textContent = "Drag & drop a picture";
-    target.classList.remove("active");
+    target2.classList.remove("active");
     testIcon.trigger = "empty";
+
+
 
     file = event.dataTransfer.files[0];
 
@@ -36,24 +38,57 @@ function upload_file(event) {
         style="width:250px;height:250px;margin-top:80px;color="#53c1ec";>
     </lord-icon><p>Classification in progress ...</p>`;
 
-    target.innerHTML = loadIcon;
-
-    // if (validExtensions.includes(fileType)) {
-    //     let fileReader = new FileReader();
-
-    //     fileReader.onload = () => {
-    //         let fileURL = fileReader.result;
-    //         let imgTag = `<img src="${fileURL}" alt="">`;
-    //         target.innerHTML = imgTag;
-    //         //containerTarget.innerHTML = imgTag;
-    //     }
-
-    //     fileReader.readAsDataURL(file);
-    // } else {
-    //     alert('The uploaded file is not an image!')
-    //     target.classList.remove("active");
-    // }
+    target2.innerHTML = loadIcon;
 }
+
+inputButton.addEventListener('click', upload_file_button());
+
+function upload_file_button() {
+
+    let reader = new FileReader()
+
+    setImagePath = e => {
+        let someURL = reader.readAsDataURL(e.target.files[0])
+
+        console.log(someURL);
+
+        reader.onload = () => {
+            this.setState({
+                queryImage: reader.result
+            })
+        }
+    }
+
+    postImage = () => {
+        fetch("https://theplantaeapi.herokuapp.com/api/v1/id", {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(this.state.queryImage)
+        })
+            .then(res => res.json())
+            .then(data => {
+                //test
+            })
+    }
+}
+
+// if (validExtensions.includes(fileType)) {
+//     let fileReader = new FileReader();
+
+//     fileReader.onload = () => {
+//         let fileURL = fileReader.result;
+//         let imgTag = `<img src="${fileURL}" alt="">`;
+//         target.innerHTML = imgTag;
+//         //containerTarget.innerHTML = imgTag;
+//     }
+
+//     fileReader.readAsDataURL(file);
+// } else {
+//     alert('The uploaded file is not an image!')
+//     target.classList.remove("active");
+// }
+
+
 
 function file_explorer() {
     inputButton.click();
