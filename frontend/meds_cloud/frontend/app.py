502da7dbdc99b86cc62a769e0e57b42163c49be1
zottelsheep <mail@zottelsheep.de>
2023-01-17 19:57:43 +0100

    Refactor: Javascript as class

--- frontend/meds_cloud/frontend/app.py
+++ frontend/meds_cloud/frontend/app.py
@@ -4,16 +4,23 @@ RufusCatCM, 30-Nov-2022
 Render the Frontend as a Flask Webapp
 """
 
+import os
 from flask import Flask, render_template
+
+if "API_ENDPOINT" in os.environ:
+    API_ENDPOINT = os.environ["API_ENDPOINT"]
+else:
+    API_ENDPOINT = "./api/predict"
 
 app = Flask(__name__, static_folder='static')
 
 
 @app.route('/')
 def render_frontend():
-    return render_template('index.html')
+    return render_template('index.html', api_endpoint=API_ENDPOINT)
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',
-            port=5000)
+            port=5000,
+            debug=True)
--- frontend/meds_cloud/frontend/static/script.js
+++ frontend/meds_cloud/frontend/static/script.js
@@ -1,206 +1,172 @@
-const dragDropZone = document.getElementById("drop_target");
-const containerTarget = document.getElementById("target_container");
-const dragText = document.getElementById("drag_header");
-const testIcon = document.getElementById("some_test");
-const inputButton = document.getElementById("input");
-const output = document.getElementById('create_output');
-const createButtonZone = document.getElementById("create_button");
-const requestButton = document.getElementById('resend_button');
-let imgFileData;
-let validExtensions = ['image/jpeg', 'image/jpg', 'image/png'];
+class app {
+  constructor(
+    api_endpoint = "./api/predict",
+    validExtensions = ['image/jpeg', 'image/jpg', 'image/png'],
+    time_to_wait_before_request = 0
+  ) {
+    // Configs
+    this.api_endpoint = api_endpoint;
+    this.validExtensions = validExtensions;
+    this.time_to_wait_before_request = time_to_wait_before_request
+    this.last_image = null;
 
-requestButton.classList.add("active");
+    // Targets
+    this.dragDropZone = document.getElementById("drop_target");
+    this.containerTarget = document.getElementById("target_container");
+    this.dragText = document.getElementById("drag_header");
+    this.inputButton = document.getElementById("input");
+    this.output = document.getElementById('output_container');
+    this.createButtonZone = document.getElementById("create_button");
+    this.requestButton = document.getElementById('request_button');
 
-function drag_over() {
-  dragText.textContent = "Release to upload";
-  dragDropZone.classList.add("active");
-  containerTarget.classList.add("active");
-  testIcon.trigger = "loop";
-}
-
-function drag_leave() {
-  dragText.textContent = "Drag & drop a picture";
-  dragDropZone.classList.remove("active");
-  containerTarget.classList.remove("active");
-  testIcon.trigger = "empty";
-}
-
-function place_loading_icon() {
-  let loadIcon = `<script src="https://cdn.lordicon.com/fudrjiwc.js"></script>
-    <lord-icon
-        src="https://cdn.lordicon.com/dpinvufc.json"
-        trigger="loop"
-        style="width:250px;height:250px;margin-top:80px;color="#53c1ec";>
-    </lord-icon><p style="padding-bottom:20px;margin-top: 50px;">Classification in progress ...</p>`;
-
-  dragDropZone.innerHTML = loadIcon;
-}
-
-function place_loading_icon_output() {
-  let loadIcon = `<script src="https://cdn.lordicon.com/fudrjiwc.js"></script>
-  <lord-icon
-      src="https://cdn.lordicon.com/dpinvufc.json"
-      trigger="loop"
-      style="width:150px;height:150px;">
-  </lord-icon><p>Resend Request ...</p>`;
-
-  const output_container = document.getElementById('output');
-
-  output_container.innerHTML = loadIcon;
-}
-
-async function upload_file(event) {
-  dragText.textContent = "Drag & drop a picture";
-  dragDropZone.classList.remove("active");
-  testIcon.trigger = "empty";
-
-  event.preventDefault();
-
-  imgFileData = event.dataTransfer.files[0];
-
-  file = event.dataTransfer.files[0];
-
-  let fileType = file.type;
-
-  // let validExtensions = ['image/jpeg', 'image/jpg', 'image/png'];
-
-  if (validExtensions.includes(fileType)) {
-    place_loading_icon();
-    setTimeout(async () => {
-      displayImages(fileType);
-      display_output(await get_prediction(file));
-      requestButton.classList.remove("active");
-    }, 0)
-  }
-  else {
-    alert('The uploaded file is not an image!')
-  }
-}
-
-async function upload_file_button(event) {
-  dragText.textContent = "Drag & drop a picture";
-  dragDropZone.classList.remove("active");
-  testIcon.trigger = "empty";
-
-  event.preventDefault();
-
-  imgFileData = event.target.files[0];
-
-  file = event.target.files[0];
-
-  let fileType = file.type;
-
-  // let validExtensions = ['image/jpeg', 'image/jpg', 'image/png'];
-
-  if (validExtensions.includes(fileType)) {
-    place_loading_icon();
-    setTimeout(async () => {
-      displayImages(fileType);
-      display_output(await get_prediction(file));
-      requestButton.classList.remove("active");
-    }, 0)
-  }
-  else {
-    alert('The uploaded file is not an image!')
-  }
-}
-
-async function resend_request_button() {
-  if (imgFileData) {
-    place_loading_icon_output();
-    setTimeout(async () => {
-      display_output(await get_prediction(imgFileData));
-    }, 0)
-  } else {
-    alert('No available request to reload. Make sure you have inserted an image first!');
-  }
-}
-
-async function get_prediction(file) {
-  var formdata = new FormData();
-  formdata.append("image", file, file.name);
-
-  var requestOptions = {
-    method: 'POST',
-    body: formdata,
-    redirect: 'follow'
-  };
-
-  const prediction = await fetch("./api/predict", requestOptions)
-    .then(response => response.json())
-    .then(data => { return data })
-    .catch(error => console.log('error', error));
-
-  return prediction['predictions']
-}
-
-function display_output(prediction) {
-  const outputString = JSON.stringify(prediction);
-
-  let outPutHTML = `
-    <div id='output' class="output_zone">
-      <div class="output_content_container">
-        <div id="labels_output"></div>
-        <div id="probability_output"></div>
-      </div>
-    </div>
-  `
-  output.innerHTML = outPutHTML;
-
-  var regex1 = /label/g;
-  var regex2 = /probability/g;
-
-  const labels = [];
-  const probabilities = [];
-
-  while ((match = regex1.exec(outputString)) != null) {
-    var firstChar = outputString.substring(match.index + 8);
-    var str = firstChar.substring(0, firstChar.indexOf('"'));
-    labels.push(str);
+    this.requestButton.classList.add("active");
   }
 
-  while ((match = regex2.exec(outputString)) != null) {
-    var firstChar = outputString.substring(match.index + 13);
-    var str = firstChar.substring(0, firstChar.indexOf('}'));
-    probabilities.push(str);
+  async get_prediction(file) {
+    var formdata = new FormData();
+    formdata.append("image", file, file.name);
+
+    var requestOptions = {
+      method: 'POST',
+      body: formdata,
+      redirect: 'follow'
+    };
+
+    const prediction = await fetch(api_endpoint, requestOptions)
+      .then(response => response.json())
+      .then(data => { return data })
+
+    return prediction['predictions']
   }
 
-  let list = document.getElementById("labels_output");
-  let list2 = document.getElementById("probability_output");
-  const underline = /_/g;
-  const subStr = ` `;
+  drag_over() {
+    this.dragText.textContent = "Release to upload";
+    this.dragDropZone.classList.add("active");
+    this.containerTarget.classList.add("active");
+  }
 
-  labels.forEach((item) => {
-    let labelList = document.createElement("div");
-    labelList.innerText = item.replace(underline, subStr) + ': ';
-    list.appendChild(labelList);
-  })
+  drag_leave() {
+    this.dragText.textContent = "Drag & drop a picture";
+    this.dragDropZone.classList.remove("active");
+    this.containerTarget.classList.remove("active");
+  }
 
-  probabilities.forEach((item) => {
-    let labelList = document.createElement("div");
-    labelList.innerText = (item * 100).toFixed(1) + " %";
-    list2.appendChild(labelList);
-  })
-}
+  place_loading_icon_output() {
+    let loadIcon = ` <lord-icon
+                        src="https://cdn.lordicon.com/dpinvufc.json"
+                        trigger="loop"
+                        style="margin-top: 35%; width:150px;height:150px;">
+                    </lord-icon>`;
 
-function displayImages(fileType) {
-  if (validExtensions.includes(fileType)) {
+    this.output.innerHTML = loadIcon;
+  }
+
+  display_output(prediction) {
+
+    // Remove all output nodes
+    this.output.innerText = ''
+
+    var table = document.createElement('table')
+    table = this.output.appendChild(table)
+
+    var lables = document.createElement('td')
+    lables = table.appendChild(lables)
+    lables.classList.add('label_output')
+
+    var probabilities = document.createElement('td')
+    probabilities = table.appendChild(probabilities)
+    probabilities.classList.add('probability_output')
+
+    prediction.forEach(element => {
+      let label = document.createElement('tr')
+      label.textContent = element.label + ': ';
+      lables.appendChild(label)
+
+      let probability = document.createElement('tr')
+      probability.textContent = (element.probability * 100).toFixed(1) + " %";
+      probabilities.appendChild(probability)
+    });
+
+
+  }
+
+  displayImage(file) {
     let fileReader = new FileReader();
 
     fileReader.onload = () => {
       let fileURL = fileReader.result;
-      let imgTag = `<div id="output_image" class="image"><img src="${fileURL}" alt="image"></div>`;
-      dragDropZone.innerHTML = imgTag;
+      let imgTag = `<div id="output_image" class="image"><img src="${fileURL}" alt="uploaded_image"></div>`;
+      this.dragDropZone.innerHTML = imgTag;
     }
 
     fileReader.readAsDataURL(file);
   }
-  else {
-    alert('The uploaded file is not an image!')
-    dragDropZone.classList.remove("active");
+
+  file_explorer() {
+    // Create an input element
+    var inputElement = document.createElement("input");
+
+    // Set its type to file
+    inputElement.type = "file";
+
+    // Set accept to the file types you want the user to select. 
+    // Include both the file extension and the mime type
+    inputElement.accept = this.validExtensions;
+
+    // set onchange event to call callback when user has selected file
+    // callback execution context needs to be bound to object
+    inputElement.addEventListener("change", this.upload_file.bind(this))
+    
+    // dispatch a click event to open the file dialog
+    inputElement.dispatchEvent(new MouseEvent("click")); 
   }
+
+  async make_prediction(file) {
+
+    this.last_image = file
+
+    this.place_loading_icon_output();
+
+    setTimeout(async () => {
+      this.displayImage(file);
+      this.display_output(await this.get_prediction(file));
+    }, this.time_to_wait_before_request)
+
+    this.requestButton.classList.remove("active");
+  }
+
+  async upload_file(event) {
+    this.dragDropZone.classList.remove("active");
+
+    event.preventDefault();
+
+    var image = null
+
+    if (event.type == "drop"){
+      image = event.dataTransfer.files[0];
+    }
+    else {
+      image = event.target.files[0];
+    }
+
+    if (!this.validExtensions.includes(image.type)) {
+      alert('The uploaded file is not an image!')
+      return
+    }
+
+    this.make_prediction(image)
+
+  }
+
+  async resend_request_button() {
+    if (!this.last_image) {
+      alert('No available request to reload. Make sure you have inserted an image first!');
+      return
+    }
+    this.make_prediction(this.last_image)
+  }
+
+
 }
 
-function file_explorer() {
-  inputButton.click();
-  }
+
